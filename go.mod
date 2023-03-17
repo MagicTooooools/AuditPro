ecd2cbc82f60e05948b755e2fce5cb3380d6a5fe
西片 <lionacc@163.com>
2023-03-17 10:51:47 +0800

    初始化项目

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/MagicTooooools/AuditPro
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("welcome to AuditPro")
+	fmt.Println("欢迎使用AuditPro")
+}
